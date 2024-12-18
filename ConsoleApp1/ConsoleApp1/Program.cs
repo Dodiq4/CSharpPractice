e94c09fb8f37e5ee965ea246a881f27fd1e4c9c9
Dodiq4 <xdeadlockedx1@gmail.com>
2024-12-18 15:26:01 +0300

    Practice Work #1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection.Metadata.Ecma335;
+
+double x, V;
+string res;
+Console.Clear();
+Console.WriteLine("Введите Х: ");
+x = Convert.ToDouble(Console.ReadLine());
+V = (Math.Exp(x) * Math.Tan(Math.Cbrt(Math.Pow(x,2)+5.3)) + Math.Pow(x,5)) / (1 + Math.Log(Math.Abs(x)));
+res = "Result: ";
+Convert.ToString(V);
+Console.WriteLine(res);
+Console.WriteLine(V);
+
+
